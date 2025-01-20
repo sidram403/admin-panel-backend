0581dd173b09cc5a62c61def70292abebfc10168
Sidramaraddy M <112577972+sidram403@users.noreply.github.com>
2025-01-20 19:10:11 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -44,7 +44,7 @@ dotenv.config();
 
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(" ")[1]; 
+    const token = await req.cookies.token || req.headers.authorization?.split(" ")[1]; 
   
   if (!token) {
     return res.status(401).json({ message: "Unauthorized: No token provided" });
